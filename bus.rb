bf31859cc09761e972430f540b36da57babcfcdc
Nadja Kelly <nmfort-info@yahoo.de>
2018-11-27 14:54:15 +0000

    initial commit and bus class created

---
+++ bus.rb
@@ -0,0 +1,24 @@
+class Bus
+
+  attr_accessor :route_number, :destination
+
+  def initialize(route_number, destination)
+    @route_number = route_number
+    @destination = destination
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
---
+++ spec/bus_spec.rb
@@ -0,0 +1,19 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../bus')
+
+class BusTest < MiniTest::Test
+
+  def setup()
+
+    @bus = Bus.new(22, "Ocean Terminal")
+  end
+
+
+
+
+
+
+end
+
+  
